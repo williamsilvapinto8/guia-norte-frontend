bba47c601f9a3de1e506b7dd35e7e150271d0cab
William Silva Pinto <william.silvapinto@gmail.com>
2026-03-18 22:07:44 -0300

    Update vite.config.js
      base: '/guia-norte-frontend/', // <-- Adicione ou ajuste esta linha

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  base: '/guia-norte-frontend/',
+  plugins: [react()],
+  base: '/guia-norte-frontend/', // <-- Adicione ou ajuste esta linha
 })
