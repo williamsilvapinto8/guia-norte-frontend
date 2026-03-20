f041c25679de30cbcd01f178e2d9220cdf65f9b1
William Silva Pinto <william.silvapinto@gmail.com>
2026-03-20 01:53:46 -0300

    Frontend build para deploy no Coolify

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // <--- Mude esta linha para um caminho relativo
+  base: '/', // <--- Mude esta linha para a raiz do domínio/subdomínio
   build: {
-    outDir: 'docs',
+    outDir: 'dist', // <--- Mude esta linha para 'dist', que é o padrão para deploy em servidores
   }
 })
